d40b287df7c9fb986120f0c2286012ff2a7db979
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-12-28 10:33:48 +0500

    first commit

---
+++ bajaruvchi/src/main/java/uz/dxm/bajaruvchi/BajaruvchiApplication.java
@@ -0,0 +1,13 @@
+package uz.dxm.bajaruvchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajaruvchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajaruvchiApplication.class, args);
+	}
+
+}
---
+++ bajaruvchi/src/test/java/uz/dxm/bajaruvchi/BajaruvchiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dxm.bajaruvchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajaruvchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
